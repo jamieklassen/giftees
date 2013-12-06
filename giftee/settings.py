2dd1d97361bbd5a768954b5d2c115069b93779b5
Jamie Klassen <michigan.j.frog@gmail.com>
2013-12-06 16:40:10 -0500

    hopefully fixes database issue

--- giftee/settings.py
+++ giftee/settings.py
@@ -1,8 +1,7 @@
 # Django settings for giftee project.
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES = {}
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
